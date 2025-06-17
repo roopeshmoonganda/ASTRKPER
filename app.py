99fa2afef6e73d0e7cc3b143f9a8be43ed9e7457
roopeshmoonganda <130567604+roopeshmoonganda@users.noreply.github.com>
2025-06-17 23:31:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,5 @@ def chat():
     return bot_reply
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
